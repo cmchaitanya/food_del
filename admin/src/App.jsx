ede803ba36c7e6503a14232ad99649cd1a386af7
cmchaitanya <157744806+cmchaitanya@users.noreply.github.com>
2024-09-26 03:20:30 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url="http://localhost:4000"
+  const url="https://foodify-backend-ddsf.onrender.com"
   return (
     <div>
       <ToastContainer/>
