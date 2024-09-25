8466546d5a63abb80a330126d5ca3d016c23a6c9
cmchaitanya <157744806+cmchaitanya@users.noreply.github.com>
2024-09-26 02:54:21 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider=(props)=>{
 
     const [food_list,setFood_list] =useState([]);
     const [cartItems,setCartItems]=useState({});
-    const url="http://localhost:4000";
+    const url="https://foodify-backend-ddsf.onrender.com";
     const [token,setToken]=useState("");
 
     const addToCart = async (itemId)=>{
